5c2a2ec0d9d05b798660cf1c46107b2e6479610d
Petr Chmelar <petr.chmelar@matee.cz>
2020-02-10 18:07:06 +0100

    Fix template for FlowController

--- templates/Devstack/FlowController.xctemplate/___FILEBASENAME___FlowController.swift
+++ templates/Devstack/FlowController.xctemplate/___FILEBASENAME___FlowController.swift
@@ -4,11 +4,11 @@ import UIKit
 
 class ___VARIABLE_productName___FlowController: ___VARIABLE_controllerSubclass___ {
     
-    override func start() {
-        super.start()
+    override func setup() {
+        super.setup()
         let vc = ___VARIABLE_productName___ViewController.instantiate()
         vc.flowDelegate = self
-        navigationController.viewControllers = [vc]
+        return vc
     }
     
 }
